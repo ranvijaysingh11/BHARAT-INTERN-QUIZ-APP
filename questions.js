3736e5f7c34fe5b91c0ad7a609288401d8cb2d3c
ranvijaysingh11 <152189422+ranvijaysingh11@users.noreply.github.com>
2024-01-12 18:30:44 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        numb:1,
+        question:"Which is the biggest continent in the world?",
+        answer:"Asia",
+        options:[
+            "North America",
+            "Asia",
+            "Australia",
+            "Africa"
+    ]
+    },
+    {
+        numb:2,
+        question:"Which is the longest river in the world?",
+        answer:"Nile",
+        options:[
+            "Great Ganga",
+            "Amazon",
+            "Nile",
+            "Niger"
+    ]
+    },
+    {
+        numb:3,
+        question:"Which is the largest ocean in the world?",
+        answer:"Pacific Ocean",
+        options:[
+            "Pacific Ocean",
+            "Indian Ocean",
+            "Arctic Ocean",
+            "Atlantic Ocean"
+    ]
+    },
+    {
+        numb:4,
+        question:"Who was India’s first President?",
+        answer:"Dr. Rajendra Prasad",
+        options:[
+            "Abdul Kalam",
+            "Dr. Rajendra Prasad",
+            "Varahagiri Venkata Giri",
+            "Narendra Modi"
+    ]
+    },
+    {
+        numb:5,
+        question:"Who is the first prime minister of India?",
+        answer:"Jawaharlal Nehru",
+        options:[
+            "Narendra Modi",
+            "Indra Gandhi",
+            "Jawaharlal Nehru",
+            "Motilal Nehru"
+    ]
+    }
+];
